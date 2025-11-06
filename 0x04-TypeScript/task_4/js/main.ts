82170c01ab738c63e666abb02582b686ee654cc4
kelv01 <kelvinnjugunanungari@gmail.com>
2025-11-06 21:42:28 +0300

    added task 4 mainjs

--- 0x04-TypeScript/task_4/js/main.ts
+++ 0x04-TypeScript/task_4/js/main.ts
@@ -1,22 +1,59 @@
 
-import { RowID, RowElement } from './interface';
+// import { RowID, RowElement } from './interface';
 
 
-import * as CRUD from './crud.js';
+// import * as CRUD from './crud.js';
 
 
-const row: RowElement = {
-  firstName: 'Guillaume',
-  lastName: 'Salva',
+// const row: RowElement = {
+//   firstName: 'Guillaume',
+//   lastName: 'Salva',
+// };
+
+
+// const newRowID: RowID = CRUD.insertRow(row);
+
+// const updatedRow: RowElement = { ...row, age: 23 };
+
+
+// CRUD.updateRow(newRowID, updatedRow);
+
+
+// CRUD.deleteRow(newRowID);
+
+
+/// <reference path="subjects/Teacher.ts" />
+/// <reference path="subjects/Subject.ts" />
+/// <reference path="subjects/Cpp.ts" />
+/// <reference path="subjects/Java.ts" />
+/// <reference path="subjects/React.ts" />
+
+// Create and export constants for subjects
+export const cpp = new Subjects.Cpp();
+export const java = new Subjects.Java();
+export const react = new Subjects.React();
+
+// Create and export the cTeacher object
+export const cTeacher: Subjects.Teacher = {
+  firstName: 'John',
+  lastName: 'Doe',
+  experienceTeachingC: 10,
 };
 
+// --- Cpp Subject ---
+console.log('C++');
+cpp.setTeacher(cTeacher);
+console.log(cpp.getRequirements());
+console.log(cpp.getAvailableTeacher());
 
-const newRowID: RowID = CRUD.insertRow(row);
+// --- Java Subject ---
+console.log('Java');
+java.setTeacher(cTeacher);
+console.log(java.getRequirements());
+console.log(java.getAvailableTeacher());
 
-const updatedRow: RowElement = { ...row, age: 23 };
-
-
-CRUD.updateRow(newRowID, updatedRow);
-
-
-CRUD.deleteRow(newRowID);
+// --- React Subject ---
+console.log('React');
+react.setTeacher(cTeacher);
+console.log(react.getRequirements());
+console.log(react.getAvailableTeacher());
